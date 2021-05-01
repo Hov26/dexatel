6f53bc3429c17332a7677ba48b86792e9078ff0d
Hov26 <hov.ghevondyan1994@gmail.com>
2021-05-01 14:07:39 +0400

    Add: office section markUp/styles

--- app/js/app.min.js
+++ app/js/app.min.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+    $('.office-slider').owlCarousel({
+        dots: false,
+        nav: true,
+        navElement: 'div',
+        items: 3,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        smartSpeed: 500,
+        responsive: {
+            320: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            }
+        }
+    });
 })
